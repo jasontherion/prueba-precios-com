3e631588f1eadc0a759e08dc9de15db41068b3e1
Jeyson <jasontherion@gmail.com>
2020-12-09 10:27:45 -0500

    NSTALACION DE WEBPACK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,29 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\Usuario;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+// Include paginator interface
+use Knp\Component\Pager\PaginatorInterface;
 
 class UserController extends AbstractController
 {
 
-    public function index(): Response
+    public function index(LoggerInterface $logger, PaginatorInterface $paginator ,Request $request): Response
     {
+        $logger->info('llamando servicio de monolog');
+        
         $date = date('Y-m-d H:i:s', strtotime("-30 days")); 
-        $response = new JsonResponse();
         $user_resp = $this->getDoctrine()->getRepository(Usuario::class);
         $query=  $user_resp->createQueryBuilder('p')
-        ->where('p.dateRegister > :dateRegister')
-        ->setParameter('dateRegister', $date)
+        ->where('p.dateregister > :dateregister')
+        ->setParameter('dateregister', $date)
         ->getQuery();
-        $user_data= $query->getResult();
+        
+        
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+           $request->query->getInt('page', 1), /*page number*/
+            $request->query->getInt('limit', 5) /*limit per page*/
+        );
 
-        foreach($user_data as $user){
-            echo "<h1> {$user->getNombre()} - {$user->getApellidos()} -{$user->getUseragent()} - {$date}</h1>";
-        }
+     #   foreach($user_data as $user){
+     #      echo "<h1> {$user->getNombre()} - {$user->getApellidos()} -{$user->getUseragent()} - {$date}</h1>";
+     #  }
 
-        die();
-
+      return $this->render('layauot.html.twig',['pagination' => $pagination]);
 
    }
 }
